1ef86eb81842fc8e46b9896800d983f23c2c0e95
AllwinJWork <123567122+AllwinJWork@users.noreply.github.com>
2023-09-11 11:57:56 +0100

    adding to dev

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React to get a job
+          Learn React to get a job to dev.
         </a>
       </header>
     </div>
